628006e0f5939842afdc08cef32311db4707ab77
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-19 12:36:21 +0300

    add nav host fragment and create navigation graph

--- app/src/main/java/com/sammy/androidarchitecture/ui/MainActivity.kt
+++ app/src/main/java/com/sammy/androidarchitecture/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.sammy.androidarchitecture
+package com.sammy.androidarchitecture.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sammy.androidarchitecture.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
