7a8e48daacb52969ea85788ba210e501b8b5b522
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-19 12:05:25 +0300

    create network module

---
+++ app/src/main/java/com/sammy/androidarchitecture/di/NetworkModule.kt
@@ -0,0 +1,29 @@
+package com.sammy.androidarchitecture.di
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.sammy.androidarchitecture.data.remote.CharactersApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(ApplicationComponent::class)
+@Module
+class NetworkModule {
+    @Singleton
+    @Provides
+    fun provideRetrofit(gson: Gson) : Retrofit = Retrofit.Builder()
+            .baseUrl("https://rickandmortyapi.com/api/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+    @Provides
+    fun provideGson(): Gson = GsonBuilder().create()
+
+    @Provides
+    fun provideCharacterService(retrofit: Retrofit): CharactersApiService = retrofit.create(CharactersApiService::class.java)
+}
