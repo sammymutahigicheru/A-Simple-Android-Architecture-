67a4b1370e920ff951d6273b2277248c9e9482ce
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-19 12:46:48 +0300

    create characters viewmodel

---
+++ app/src/main/java/com/sammy/androidarchitecture/ui/characters/CharactersViewModel.kt
@@ -0,0 +1,11 @@
+package com.sammy.androidarchitecture.ui.characters
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+import com.sammy.androidarchitecture.data.repository.CharacterRepository
+
+class CharactersViewModel @ViewModelInject constructor(
+    private val characterRepository: CharacterRepository
+):ViewModel(){
+    val getCharacters = characterRepository.getCharacters()
+}
