d84a3bc4fe9abeb6c45218d45ff2210d59a370bb
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-21 11:06:38 +0300

    modify fragment_character_details

--- app/src/main/java/com/sammy/androidarchitecture/ui/character_details/CharacterDetailsFragment.kt
+++ app/src/main/java/com/sammy/androidarchitecture/ui/character_details/CharacterDetailsFragment.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import com.sammy.androidarchitecture.R
+import com.sammy.androidarchitecture.commons.autoCleared
+import com.sammy.androidarchitecture.databinding.FragmentCharacterDetailsBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CharacterDetailsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private var binding:FragmentCharacterDetailsBinding by autoCleared()
+    private val viewModel:CharacterDetailViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,6 +24,11 @@ class CharacterDetailsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_character_details, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_character_details, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
