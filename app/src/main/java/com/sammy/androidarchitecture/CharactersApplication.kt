87fbe0722531b19b85951b3fb98453b2e04354f1
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-21 20:19:25 +0300

    initialize timber

--- app/src/main/java/com/sammy/androidarchitecture/CharactersApplication.kt
+++ app/src/main/java/com/sammy/androidarchitecture/CharactersApplication.kt
@@ -2,6 +2,17 @@ package com.sammy.androidarchitecture
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class CharactersApplication : Application()
+class CharactersApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        initTimber()
+    }
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
--- app/src/main/java/com/sammy/androidarchitecture/ui/character_details/CharacterDetailsFragment.kt
+++ app/src/main/java/com/sammy/androidarchitecture/ui/character_details/CharacterDetailsFragment.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
@@ -28,7 +27,7 @@ class CharacterDetailsFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_character_details, container, false)
         return binding.root
