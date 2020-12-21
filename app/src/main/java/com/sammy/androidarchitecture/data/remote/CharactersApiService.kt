62c6b155261cfddd606043419e8da5f7448a38c4
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-21 10:43:12 +0300

    add get character by id request to api service

--- app/src/main/java/com/sammy/androidarchitecture/data/remote/CharactersApiService.kt
+++ app/src/main/java/com/sammy/androidarchitecture/data/remote/CharactersApiService.kt
@@ -3,8 +3,11 @@ package com.sammy.androidarchitecture.data.remote
 import com.sammy.androidarchitecture.data.model.CharacterResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface CharactersApiService {
     @GET("character")
     suspend fun getAllCharacters():Response<CharacterResponse>
+    @GET("character/{id}")
+    suspend fun getCharacter(@Path("id") id: Int): Response<Character>
 }
