e894509775fe9c2625307ac24a5eda2b8ce78dea
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-15 10:53:51 +0300

    add resources directory in test folder

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath ("com.android.tools.build:gradle:7.0.0-alpha02")
+        classpath ("com.android.tools.build:gradle:4.1.1")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.20")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.28-alpha")
 
